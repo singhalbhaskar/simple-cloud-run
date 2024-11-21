6e62f720c425eb20af3138c8837116d998af879e
singhalbhaskar <singhalbhaskar@google.com>
2024-11-21 18:35:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,11 @@
-/**
- * Copyright 2021 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-module "service_account" {
-  source     = "terraform-google-modules/service-accounts/google"
-  version    = "~> 4.2"
-  project_id = var.project_id
-  prefix     = "sa-cloud-run"
-  names      = ["simple"]
-}
-
-module "cloud_run" {
-  source  = "GoogleCloudPlatform/cloud-run/google"
-  version = "~> 0.13"
-
-  service_name          = "ci-cloud-run"
-  project_id            = var.project_id
-  location              = "us-central1"
-  image                 = "us-docker.pkg.dev/cloudrun/container/hello"
-  service_account_email = module.service_account.email
+module "three_tier_app_cache" {
+  source                  = "github.com/terraform-google-modules/terraform-google-memorystore"
+  project                 = var.project_id
+  region                  = var.region
+  name                    = var.three_tier_app_cache_name
+  memory_size_gb          = var.three_tier_app_cache_memory_size_gb
+  redis_version           = var.three_tier_app_cache_redis_version
+  connect_mode            = var.three_tier_app_cache_connect_mode
+  tier                    = var.three_tier_app_cache_tier
+  transit_encryption_mode = var.three_tier_app_cache_transit_encryption_mode
 }
