60bce53d4bb098e87c533936c1b581c337de566d
singhalbhaskar <singhalbhaskar@google.com>
2024-11-21 18:35:35 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,145 @@
-/**
- * Copyright 2021 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+# Root level variables, no prefix of module name
 variable "project_id" {
-  description = "The project ID to deploy to"
   type        = string
+  description = "GCP Project ID"
+}
+
+variable "region" {
+  type        = string
+  description = "GCP Region"
+}
+
+# Variables for module three_tier_app_cache
+variable "three_tier_app_cache_name" {
+  type        = string
+  description = "Redis instance name"
+}
+
+variable "three_tier_app_cache_redis_version" {
+  type        = string
+  description = "The version of Redis software"
+}
+
+variable "three_tier_app_cache_memory_size_gb" {
+  type        = number
+  default     = 1
+  description = "Redis memory size in GiB"
+}
+
+variable "three_tier_app_cache_connect_mode" {
+  type    = string
+  default = null
+}
+
+variable "three_tier_app_cache_tier" {
+  type    = string
+  default = "STANDARD_HA"
+}
+
+variable "three_tier_app_cache_transit_encryption_mode" {
+  type    = string
+  default = "SERVER_AUTHENTICATION"
+}
+
+# Variables for module three_tier_app_database
+variable "three_tier_app_database_db_name" {
+  type        = string
+  description = "Database name"
+}
+
+variable "three_tier_app_database_name" {
+  type        = string
+  description = "Database instance name"
+}
+
+variable "three_tier_app_database_database_version" {
+  type        = string
+  description = "Database version"
+}
+
+variable "three_tier_app_database_database_flags" {
+  type    = list(object({ name : string, value : string }))
+  default = []
+}
+
+variable "three_tier_app_database_disk_size" {
+  type        = number
+  default     = 10
+  description = "The size of data disk in GB"
+}
+
+variable "three_tier_app_database_ip_configuration" {
+  type = object({ ipv4_enabled : bool })
+}
+
+variable "three_tier_app_database_deletion_protection" {
+  type    = bool
+  default = true
+}
+
+variable "three_tier_app_database_user_deletion_policy" {
+  type    = string
+  default = null
+}
+
+variable "three_tier_app_database_database_deletion_policy" {
+  type    = string
+  default = null
+}
+
+variable "three_tier_app_database_enable_default_user" {
+  type    = bool
+  default = true
+}
+
+# Variables for module three_tier_app_backend
+variable "three_tier_app_backend_service_name" {
+  type        = string
+  description = "Backend cloud Run service name"
+}
+
+variable "three_tier_app_backend_members" {
+  type    = list(string)
+  default = []
+}
+
+variable "three_tier_app_backend_template_scaling" {
+  type = object({ max_instance_count : number })
+}
+
+
+variable "three_tier_app_backend_containers" {
+  type = list(object({ container_image : string, ports : object({ container_port : number }), env_vars: map(string) }))
+}
+
+variable "three_tier_app_backend_vpc_access" {
+  type = object({
+    connector = optional(string)
+    egress    = optional(string)
+    network_interfaces = optional(object({
+      network    = optional(string)
+      subnetwork = optional(string)
+      tags       = optional(list(string))
+    }))
+  })
+}
+
+variable "three_tier_app_backend_service_account_project_roles" {
+  type    = list(string)
+  default = []
+}
+
+# Variables for module three_tier_app_frontend
+variable "three_tier_app_frontend_service_name" {
+  type        = string
+  description = "Cloud Run service name"
+}
+
+variable "three_tier_app_frontend_members" {
+  type    = list(string)
+  default = []
+}
+
+variable "three_tier_app_frontend_containers" {
+  type = list(object({ container_image : string, ports : object({ container_port : number }) }))
 }
